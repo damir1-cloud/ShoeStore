6550188ba4246985b1ffb2af51135ce16d5a24a2
damir1-cloud <66561971+damir1-cloud@users.noreply.github.com>
2020-08-27 14:15:50 +0500

    first commit

---
+++ src/Components/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRoter as Router} from "react-router-dom";
+
+const Rnew = ()=> {
+
+
+
+return (
+        <div>hellow world</div>
+);
+
+
+}
+
+export default Rnew();
